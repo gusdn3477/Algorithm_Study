51714cabd0390c3105084aec73308ce9d1696b46
박현우 <gusdn3477@naver.com>
2020-11-07 16:28:25 +0900

    11/7 문제풀이 추가
    11/7 문제풀이 추가

---
+++ 프로그래머스/완주하지못한선수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	int i;
+	vector<string>::iterator iter;
+
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+
+	for (i = 0; i < completion.size(); i++) {
+		if (participant[i] != completion[i]) {
+			answer = participant[i];
+			return answer;
+		}
+	}
+
+	answer = participant[participant.size() - 1];
+	return answer;
+}

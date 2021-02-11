765271667e03cd44cfe0f5b80c41eab534008403
박현우 <gusdn3477@naver.com>
2021-02-11 21:34:03 +0900

    20210211
    문제풀이 추가

---
+++ baekjoon/18000~20999/20540_연길이의이상형.py
@@ -0,0 +1,26 @@
+s = input()
+
+for i in s:
+    if i == 'E':
+        print('I', end='')
+
+    if i == 'I':
+        print('E', end='')
+
+    if i == 'S':
+        print('N', end='')
+
+    if i == 'N':
+        print('S', end='')
+
+    if i == 'T':
+        print('F', end='')
+
+    if i == 'F':
+        print('T', end='')
+
+    if i == 'J':
+        print('P', end='')
+
+    if i == 'P':
+        print('J', end='')
